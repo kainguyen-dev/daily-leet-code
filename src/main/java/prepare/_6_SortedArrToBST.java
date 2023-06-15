cd2366e0e66c50b91c371a5dc01bcdf8e0ae7655
Duyntc <congduy@us.maxnerva.com>
2023-06-15 17:01:57 +0700

    Update PREPARE

--- src/main/java/prepare/_6_SortedArrToBST.java
+++ src/main/java/prepare/_6_SortedArrToBST.java
@@ -6,4 +6,37 @@ public class _6_SortedArrToBST {
      * 7.	https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/. Should run in O(N)
      */
 
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    public TreeNode helper(int left, int right, int[] nums) {
+        if (left > right) return null;
+        int mid = left + (right - left) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = helper(left, mid - 1, nums);
+        node.right = helper(mid + 1, right, nums);
+        return node;
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(0, nums.length - 1, nums);
+    }
 }
--- src/main/java/prepare/_8_TrappingRainWater.java
+++ src/main/java/prepare/_8_TrappingRainWater.java
@@ -3,4 +3,36 @@ package prepare;
 public class _8_TrappingRainWater {
 
     // https://leetcode.com/problems/trapping-rain-water/
+
+    public int trap(int[] height) {
+
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int result = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    // We can add some water
+                    result += (leftMax - height[left]);
+                }
+
+                left++;
+            } else {
+
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    // We can add some water
+                    result += (rightMax - height[right]);
+                }
+
+                right--;
+            }
+        }
+
+        return result;
+    }
 }
